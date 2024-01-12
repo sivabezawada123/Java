bd84670211c4ee6dc7398cdc979fd21d3efdfc8a
ShivaKumar <sivakumarbezawada11@gmail.com>
2024-01-12 11:04:21 -0600

    updated push

---
+++ Bike.java
@@ -0,0 +1,7 @@
+public class Bike extends Vehicle {
+    @Override
+    public void move(){
+        System.out.println(" The Bike is moving");
+
+    }
+}
---
+++ SubDepartment.java
@@ -0,0 +1,9 @@
+public class SubDepartment extends Department {
+
+    @Override
+    int getDepartmenSize() {
+        // TODO Auto-generated method stub
+        int DeptSize = 100;
+        return DeptSize;
+    }
+}
