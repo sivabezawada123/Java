7cf2b0456bcc599a97b5408bcfd557f334b8d6df
sivabezawada123 <156051466+sivabezawada123@users.noreply.github.com>
2024-01-17 12:33:06 -0600

    new files upload

---
+++ Loops and conditions/Fizzbuzz.java
@@ -0,0 +1,20 @@
+public class Fizzbuzz {
+
+    public static void fizz() {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println("Appreciated");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        fizz();
+    }
+}
